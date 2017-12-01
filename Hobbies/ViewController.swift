308caf976ba121a23f28cdfd15bcff794186f79f
jamie <jchavez@lincolnschool.org>
2017-12-01 14:24:57 -0500

    Initial Commit

---
+++ Hobbies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hobbies
+//
+//  Created by Student on 12/1/17.
+//  Copyright © 2017 Jamie Chavez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
